37b5a1c77ff1a19704a29849be403023645580b1
BaskaranAathithyan <itsthiyan11@gmail.com>
2022-01-28 18:47:10 +0530

    expressjs

---
+++ One.js
@@ -0,0 +1,12 @@
+/* 
+//Local Module
+class myClass{
+    constructor(name){
+        this.name=name;
+    }
+    myFunc(){
+        console.log('my name is:' + this.name);
+    }
+}
+
+module.exports.myClass=myClass; */
---
+++ index.js
@@ -0,0 +1,37 @@
+/*
+Local Module
+const global=require('./One');
+
+
+var obj=new global.myClass( );
+
+obj.myFunc();
+*/
+
+//core Module
+/* const readline= require('readline');
+
+const rl =readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('what is Your Nmae?', (ans) =>{
+    if(ans=='aa'){
+        rl.close();
+    }else{
+        rl.setPrompt('wrong ');
+        rl.prompt();
+        rl.on('line' ,(inp) => {
+            if(inp=='aa'){
+                rl.close();
+            }else{
+                rl.setPrompt('wrong' + inp);
+                rl.prompt();
+            }
+        })
+    }
+    
+    
+}); */
+
